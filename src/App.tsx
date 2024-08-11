552397480f83dc717e936bddfda2296063fd5d00
Robert Szczygielski <szczygielski@dicedev.pl>
2024-08-11 09:26:36 +0200

    add user form

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,24 @@ import './App.css'
 import {Layout} from "antd";
 import {Content} from "antd/es/layout/layout";
 import LoginForm from "./login/LoginForm.tsx";
+import UserAddForm from "./addUser/UserAddForm.tsx";
+import MainView from "./mainView/MainView.tsx";
 
 function App() {
 
+    const token = localStorage.getItem('authToken');
 
     return (
         <>
-            <Layout style={{ height: '100vh', justifyContent: 'center', alignItems: 'center' }}>
-                <Content>
-                    <LoginForm />
-                </Content>
-            </Layout>
+            { token ? (
+                <MainView token={token} />
+                    ) : (
+                <Layout style={{ height: '100vh', justifyContent: 'center', alignItems: 'center' }}>
+                    <Content>
+                        <LoginForm />
+                    </Content>
+                </Layout>
+                )}
         </>
     )
 }
